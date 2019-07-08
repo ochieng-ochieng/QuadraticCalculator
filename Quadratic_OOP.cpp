c85b6c3cd3df21659bd37542b8181ed30ac20df8
EDDIE  OCHIENG <edwardochieng2010@gmail.com>
2019-07-08 14:48:52 +0300

    Add files via upload

---
+++ Quadratic_OOP.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<cstdlib>
+#include<math.h>
+
+using namespace  std;
+
+class Quadratic{
+    float a, b, c;
+public:
+    Quadratic(){}
+    Quadratic(float a1, float b1, float c1)
+    {
+        a = a1;
+        b = b1;
+        c = c1;
+    }
+    void set_a(float a1)
+    {
+        if(a1 == 0)
+            throw a1;
+
+        a = a1;
+    }
+    float get_a()
+    {
+        return a;
+    }
+
+    void set_b(float b1)
+    {
+        b = b1;
+    }
+    float get_b()
+    {
+        return b;
+    }
+
+    void set_c(float c1)
+    {
+        c = c1;
+    }
+    float get_c()
+    {
+        return c;
+    }
+
+    float deltaFunc();
+};
+
+float Quadratic::deltaFunc()
+{
+    return  (b * b - (4 * a * c));
+}
+
+int main()
+{
+    Quadratic  q1;
+    float a2, b2, c2, x1, x2;
+
+    cout << "Enter variable a: ";
+    cin >>a2;
+    try{
+        q1.set_a(a2);
+
+    }catch(float a2){
+        cout << "Error!! a must not be " << a2 << endl;
+    }
+
+
+    cout << "Enter variable b: ";
+    cin >>b2;
+    q1.set_b(b2);
+
+    cout << "Enter variable c: ";
+    cin>>c2;
+    q1.set_c(c2);
+
+    if (q1.deltaFunc() < 0)
+    {
+        cout<<"The equation has no answer!\n";
+        exit(0);
+    }
+
+    else if (q1.deltaFunc() == 0)
+    {
+        x1 = -q1.get_b()/(2 * q1.get_a());
+        cout << "The equation has two equal answers !\n";
+        cout << "x1 = x2 = " << x1 << endl;
+        //exit(0);
+    }
+
+    else{
+
+        x1 = (-q1.get_b() + sqrt(q1.deltaFunc()))/(2 * q1.get_a());
+        x2 = (-q1.get_b() - sqrt(q1.deltaFunc()))/(2 * q1.get_a());
+        cout << "\nX1 = " << x1;
+        cout << "\nX2 = " << x2 << endl;
+
+    }
+
+
+    return 0;
+}
